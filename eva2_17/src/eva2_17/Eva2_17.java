1f4c7542b8d14bf2c9f0a3aafd780dce04218260
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-10-24 16:45:53 -0600

    Add files via upload

---
+++ eva2_17/src/eva2_17/Eva2_17.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva2_17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String diasemana[]={"lunes","martes","miercoles","jueves","viernes","sabado",
+        "domingo"};
+        int valores[]={20,50,100,200,500,1000};
+        for(int i=0; i<diasemana.length; i++){System.out.println(diasemana[i]+"-");}
+        System.out.println("");
+        for(int i=0; i< valores.length; i++){System.out.println(valores[i]+"-");}
+    }
+    
+}
