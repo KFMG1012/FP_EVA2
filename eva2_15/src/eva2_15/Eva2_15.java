f5985cda9f7ec6675caf4c1075d9fd0c5c9fbb24
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-10-24 16:34:08 -0600

    Add files via upload

---
+++ eva2_15/src/eva2_15/Eva2_15.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva2_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARACION DE ARREGLOS:
+        int[]arreglo = new int[10];//ocupa 40 bytes
+        //manejo por indices
+        //primer elemento de un arrreglo es el-->0
+        arreglo[0]=100;
+        arreglo[1]=200;
+        arreglo[2]=300;
+        arreglo[3]=400;
+        arreglo[4]=500;
+        arreglo[5]=600;
+        arreglo[6]=700;
+        arreglo[7]=800;
+        
+        
+        //ultimo elemnto de un arrglo-->tamaño-1
+        arreglo[8]=900;
+        arreglo[9]=1000;
+ System.out.println(arreglo[0]);
+  System.out.println(arreglo[1]);
+    System.out.println(arreglo[2]);
+     System.out.println(arreglo[3]);
+       System.out.println(arreglo[4]);
+          System.out.println(arreglo[5]);
+            System.out.println(arreglo[6]);
+              System.out.println(arreglo[7]);
+                System.out.println(arreglo[8]);
+                   System.out.println(arreglo[9]);
+    }
+}
